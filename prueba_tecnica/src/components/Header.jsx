82fc8cc800244baf5225fd3d086b97bb94a75dd6
vendetta98 <nelsontrejo437@gmail.com>
2024-11-25 11:09:10 -0600

    creacion de header, y archivos de configuracion

---
+++ prueba_tecnica/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../components/style/header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1>Prueba Tecnica</h1>
+      </div>
+    </>
+  );
+}
+
+export default Header;
